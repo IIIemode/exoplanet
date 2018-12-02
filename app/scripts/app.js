9c2646b308b4643121f45b01bb34ac0b487c750b
IIIemode <deniszhibenkov@qip.ru>
2018-12-02 03:17:03 +0300

    Rewrite get with the Fetch API Finish the getJSON method Test by console Handle errors by passing "unknown" to addSearchHeader

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,12 @@
+/*
+Instructions:
+(1) Rewrite get with the Fetch API: https://davidwalsh.name/fetch
+(2) Finish the getJSON method. getJSON should take a URL and return the parsed JSON response.
+  (a) getJSON needs to return a Promise!
+(3) Test by console.logging the response and by passing the query string from getJSON to addSearchHeader.
+(4) Handle errors by passing "unknown" to addSearchHeader.
+ */
+
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
@@ -6,45 +15,37 @@
 
   var home = null;
 
-  function addSearchHeader(response) {
-    try {
-      response = JSON.parse(response).query;
-    } catch (e) {
-      // it's 'unknown', so leave it alone
-    }
-    home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
   }
 
   function get(url) {
-    /*
-    This code needs to get wrapped in a Promise!
-     */
-    var req = new XMLHttpRequest();
+    return fetch(url, {
+      method: 'get'
+    });
+  }
 
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
+  function getJSON(url) {
+    return get(url).then(function(response) {
+      // Handle network errors
+      if (!response.ok) {
+        throw Error(response.statusText ? response.statusText : 'Unknown network error');
       }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+
+      return response.json();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line you're ready to start chaining and testing!
-    You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
-    pass 'unknown' to addSearchHeader if it rejects.
-     */
-    // get('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function(response) {
+      addSearchHeader(response.query);
+      console.log(response);
+    })
+    .catch(function(error) {
+      addSearchHeader('unknown');
+      console.log(error);
+    });
   });
 })(document);
