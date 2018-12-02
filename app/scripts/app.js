a3ed32526097df19343597f2c39b8d0a097376a0
IIIemode <deniszhibenkov@qip.ru>
2018-12-02 03:08:40 +0300

    fix text

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,3 @@
-/*
-Instructions:
-(1) Wrap an XHR in a Promise in the get() function below. See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-  (a) Resolve on load and reject on error.
-(2) If the XHR resolves, use addSearchHeader to add the search header to the page.
-(3) If the XHR fails, console.log the error and pass 'unknown' to addSearchHeader
- */
-
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
@@ -14,24 +6,15 @@ Instructions:
 
   var home = null;
 
-  /**
-   * Helper function to show the search query.
-   * @param {String} response - The unparsed JSON response from get.
-   */
   function addSearchHeader(response) {
     try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
+      response = JSON.parse(response).query;
     } catch (e) {
       // it's 'unknown', so leave it alone
     }
     home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
   }
 
-  /**
-   * XHR wrapped in a promise.
-   * @param  {String} url - The URL to fetch.
-   * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
-   */
   function get(url) {
     /*
     This code needs to get wrapped in a Promise!
