f423e6a3353e56816c805fd3f4ffac7f2d7ccc5c
IIIemode <deniszhibenkov@qip.ru>
2018-12-02 03:38:51 +0300

    Create Promises in the proper order even if all the requests haven`t finished

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,9 @@
+/*
+Challenge:
+Use what you've learned about Promises to request thumbnails in parallel but create them in the
+proper order even if all the requests haven't finished.
+ */
+
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
@@ -6,45 +12,75 @@
 
   var home = null;
 
-  function addSearchHeader(response) {
-    try {
-      response = JSON.parse(response).query;
-    } catch (e) {
-      // it's 'unknown', so leave it alone
-    }
-    home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+  }
+
+  function createPlanetThumb(data) {
+    return new Promise(function(resolve) {
+      var pT = document.createElement('planet-thumb');
+
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+
+      home.appendChild(pT);
+      console.log('rendered: ' + data.pl_name);
+      resolve();
+    });
   }
 
   function get(url) {
-    /*
-    This code needs to get wrapped in a Promise!
-     */
-    var req = new XMLHttpRequest();
+    return fetch(url);
+  }
 
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
+  function getJSON(url) {
+    console.log('sent: ' + url);
+
+    return get(url).then(function(response) {
+      // For testing purposes, I'm making sure that the urls don't return in order
+      if (url === 'data/planets/Kepler-62f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url);
+            
+            resolve(response.json());
+          }, 500);
+        });
       } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
+        console.log('received: ' + url);
+
+        return response.json();
       }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line you're ready to start chaining and testing!
-    You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
-    pass 'unknown' to addSearchHeader if it rejects.
-     */
-    // get('../data/earth-like-results.json')
+
+    getJSON('../data/earth-like-results.json')
+    .then(function(response) {
+      addSearchHeader(response.query);
+      return response;
+    })
+    .then(function(response) {
+      var sequence = Promise.resolve();
+
+      // .map executes all of the network requests immediately.
+      var arrayOfExecutingPromises = response.results.map(function(result) {
+        return getJSON(result);
+      });
+
+      arrayOfExecutingPromises.forEach(function (request) {
+        // Loop through the pending requests that were returned by .map (and are in order) and
+        // turn them into a sequence.
+        // request is a getJSON() that's currently executing.
+        sequence = sequence.then(function() {
+          // Remember that createPlanetThumb is a Promise, so it must resolve before Promises
+          // later in the sequence can execute.
+          return request.then(createPlanetThumb);
+        });
+      });
+    });
   });
 })(document);
